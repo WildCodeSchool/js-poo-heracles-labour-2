e1d50ac09c0780f5053ff7cb74a2800f4d2bc4a9
Romain Guillemot <rocambille@gmail.com>
2022-04-06 17:39:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const Fighter = require("./src/Fighter.js");
 
-/** Création d'Héraclès  */
+/** Create Heracles  */
 const heracles = new Fighter("🧔 Heracles", 20, 6);
 
-/** Création de l'adversaire  */
+/** Create the opponent  */
 const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
 
 /**
- * Permet d'afficher le résultat de l'affrontement
+ * Helper to produce the result of a round
  */
 const roundDisplay = (fighter1, fighter2) => {
   return `${fighter1.name} 🗡️  ${fighter2.name} 💙 ${fighter2.name} : ${fighter2.life}`;
 };
 
 /**
- * Permet de déterminer le winner et le looser du combat
+ * Helper to dispatch fighters between winner and loser
  */
 const score = (fighter1, fighter2) => {
   return fighter1.isAlive() ? {
     winner: fighter1,
-    looser: fighter2,
+    loser: fighter2,
   } : {
     winner: fighter2,
-    looser: fighter1
+    loser: fighter1
   };
 };
